e0cca645d6d1e238a2be354c5389c2c3daad9d28
Tony Tran <codemonkey713@gmail.com>
2021-06-15 23:21:25 -0500

    cleaned up files for initial cleanup of project

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
-// function to get the current date amd time
 function getCurrentDate() {
   let currentDate = moment().format('MMMM Do YYYY, h:mma');
-  $("#currentDay").text(currentDate);
+  $('#currentDay').text(currentDate);
 }
 
 getCurrentDate();
@@ -9,87 +8,89 @@ getCurrentDate();
 // array of objects for each time slot
 let myDay = [
   {
-    id: "0",
-    hour: "08",
-    time: "08",
-    ampm: "am",
-    notes: "You are awesome!"
+    id: '0',
+    hour: '08',
+    time: '08',
+    ampm: 'am',
+    notes: 'You are awesome!',
   },
   {
-    id: "1",
-    hour: "09",
-    time: "09",
-    ampm: "am",
-    notes: ""
+    id: '1',
+    hour: '09',
+    time: '09',
+    ampm: 'am',
+    notes: '',
   },
   {
-    id: "2",
-    hour: "10",
-    time: "10",
-    ampm: "am",
-    notes: ""
+    id: '2',
+    hour: '10',
+    time: '10',
+    ampm: 'am',
+    notes: '',
   },
   {
-    id: "3",
-    hour: "11",
-    time: "11",
-    ampm: "am",
-    notes: ""
+    id: '3',
+    hour: '11',
+    time: '11',
+    ampm: 'am',
+    notes: '',
   },
   {
-    id: "4",
-    hour: "12",
-    time: "12",
-    ampm: "pm",
-    notes: ""
+    id: '4',
+    hour: '12',
+    time: '12',
+    ampm: 'pm',
+    notes: '',
   },
 
   // time will be using 24-hour format so it can be calculated easier while display time will be using AM/PM
   {
-    id: "5",
-    hour: "01",
-    time: "13",
-    ampm: "pm",
-    notes: ""
+    id: '5',
+    hour: '01',
+    time: '13',
+    ampm: 'pm',
+    notes: '',
   },
   {
-    id: "6",
-    hour: "02",
-    time: "14",
-    ampm: "pm",
-    notes: ""
+    id: '6',
+    hour: '02',
+    time: '14',
+    ampm: 'pm',
+    notes: '',
   },
   {
-    id: "7",
-    hour: "03",
-    time: "15",
-    ampm: "pm",
-    notes: ""
+    id: '7',
+    hour: '03',
+    time: '15',
+    ampm: 'pm',
+    notes: '',
   },
   {
-    id: "8",
-    hour: "04",
-    time: "16",
-    ampm: "pm",
-    notes: ""
+    id: '8',
+    hour: '04',
+    time: '16',
+    ampm: 'pm',
+    notes: '',
   },
   {
-    id: "9",
-    hour: "05",
-    time: "17",
-    ampm: "pm",
-    notes: ""
-  }
+    id: '9',
+    hour: '05',
+    time: '17',
+    ampm: 'pm',
+    notes: '',
+  },
 ];
 
 // this is to draw the hourly time slots
-myDay.forEach(function(thisHR){
-  const hourRow = $('<div>').attr({'class': 'row time-block'});
+myDay.forEach(function (thisHR) {
+  const hourRow = $('<div>').attr({ class: 'row time-block' });
   $('.container').append(hourRow);
 
-  const hourEl = $('<div>').text(`${thisHR.hour}${thisHR.ampm}`).attr({'class': 'col-sm-12 col-md-2 hour'});
+  const hourEl = $('<div>')
+    .text(`${thisHR.hour}${thisHR.ampm}`)
+    .attr({ class: 'col-sm-12 col-md-2 hour' });
 
-  const hourDesc = $('<div>').attr({'class': 'col-sm-12 col-md-9 description'});
+  const hourDesc = $('<div>').attr({ class: 'col-sm-12 col-md-9 description' });
   const hourDescText = $('<textarea>');
   hourEl.append(hourDesc);
   hourDesc.append(hourDescText);
@@ -97,16 +98,16 @@ myDay.forEach(function(thisHR){
 
   // this sets the color code for past, present, future event text element
   if (thisHR.time < moment().format('HH')) {
-    hourDescText.attr ({'class': 'past'})
+    hourDescText.attr({ class: 'past' });
   } else if (thisHR.time === moment().format('HH')) {
-    hourDescText.attr ({'class': 'present'})
+    hourDescText.attr({ class: 'present' });
   } else if (thisHR.time > moment().format('HH')) {
-    hourDescText.attr ({'class': 'future'})
-  };
+    hourDescText.attr({ class: 'future' });
+  }
 
   // this is the save button
   const saveBtn = $("<i class='far fa-save fa-lg'></i>");
-  const saveArea = $('<button>').attr({'class': 'col-sm-12 col-md-1 saveBtn'});
+  const saveArea = $('<button>').attr({ class: 'col-sm-12 col-md-1 saveBtn' });
   saveArea.append(saveBtn);
 
   hourRow.append(hourEl, hourDesc, saveArea);
@@ -129,8 +130,8 @@ function saveNotes() {
 // function to add the notes to the description
 function displayNotes() {
   myDay.forEach(function (thisNote) {
-      $(`#${thisNote.id}`).val(thisNote.notes);
-  })
+    $(`#${thisNote.id}`).val(thisNote.notes);
+  });
 }
 
 // function for save event button
